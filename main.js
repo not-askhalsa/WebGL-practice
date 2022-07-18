68b09c9ea0be39ec25056d6ac017f92bd51bad8c
arsh <arshdeep.singh@crownstack.com>
2022-07-18 15:57:25 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+window.onload = function main() {
+    const canvas = document.querySelector("#glCanvas");
+    const gl = canvas.getContext("webgl");
+  
+    console.log(gl)
+
+    if (gl === null) {
+      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+    }
+  
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+  
+//    = main;
